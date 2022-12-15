bd0b15ec3bf564c48ae07d889449617421c31c6c
ilkin <huseynlilkin@gmail.com>
2022-12-15 22:41:08 +0100

    day-10 super ugly solution and input added

--- day-10/solution.py
+++ day-10/solution.py
@@ -1,4 +1,49 @@
+import numpy as np
+
 important_cycles = [20, 60, 100, 140, 180, 220]
+
+
+def print_scr(screen, sprt, crt_x, crt_y):
+    print(f'CRT draws pixel in position {sprt}')
+    print(f'Sprite position: {crt_x} {crt_y}')
+    print()
+
+    if crt_y - 1 <= sprt <= crt_y + 1:
+        screen[crt_x][crt_y] = 1
+
+
+def get_coordinates(cycle):
+    row = cycle // 40
+    col = cycle - row * 40
+    return row, col
+
+
+def part_2():
+    fp = open('input.txt')
+    screen = np.zeros((6, 40))
+    cycle = 0
+    sprite_pos = 1
+    for line in fp.readlines():
+        row, col = get_coordinates(cycle)
+        line = line.strip()
+
+        print_scr(screen, sprite_pos, row, col)
+
+        if line == 'noop':
+            cycle += 1
+        else:
+            row, col = get_coordinates(cycle + 1)
+            print_scr(screen, sprite_pos, row, col)
+
+            val = int(line.split()[-1])
+            sprite_pos += val
+            cycle += 2
+
+    screen = screen.reshape(6, 40)
+    for row in screen:
+        for item in row:
+            print(' . ' if item == 0 else ' # ', end='')
+        print()
 
 
 def main():
@@ -32,11 +77,11 @@ def main():
             tc = cycle
             tx = x
 
-        print(f'Cycle: {tc} * {tx} = {tc*tx}')
+        print(f'Cycle: {tc} * {tx} = {tc * tx}')
 
-        signal_strength += tc*tx
+        signal_strength += tc * tx
     print(signal_strength)
 
 
 if __name__ == '__main__':
-    main()
+    part_2()
